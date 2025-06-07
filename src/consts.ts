8136d4833f4633f549ebb78ad68fe95f2df99df0
Shinyvv <shinyadaniel0@gmail.com>
2025-06-07 13:13:53 -0400

    Página, primer commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Banda Fotográfica Talagante';
+export const SITE_DESCRIPTION = '¡Bienvenidos a nuestro local!';
